36acbc5a14822a35a965d793886da7785342da2d
fan-qiang <fanqiang1985@outlook.com>
2018-07-27 10:11:01 +0800

    初始提交

---
+++ step-by-step/task5-script/demo.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+let fs = require("fs")
+
+const separator = "/"
+const currentDir = "." + separator
+const fileSet = 
+[{ name: "index.html", relativePath: "", template: "<!DOCTYPE><title>Hello</title><h1>Hi</h1>" },
+{ name: "style.css", relativePath: "css", template: "h1{color: red;}" },
+{ name: "main.js", relativePath: "js", template: 'var string = "Hello World" \n alert(string)' }]
+
+let dirName = process.argv[2]
+//创建主目录
+createAppDir(dirName); 
+//进入主目录
+process.chdir(currentDir + dirName);
+//写入文件
+wirteFileSet(fileSet)
+process.exit(0)
+
+function createAppDir(dirName) {
+    assertNotEmptyArg(dirName);
+    assertNotExists(dirName);
+    fs.mkdirSync(currentDir + dirName);
+}
+
+
+function wirteFileSet(fileSet) {
+    fileSet.forEach(f => {
+        let filePath = "";
+        if (f.relativePath) {
+            fs.mkdirSync(f.relativePath);
+            filePath = f.relativePath;
+        }
+        filePath = [currentDir, filePath, separator, f.name].join("");
+        fs.writeFileSync(filePath, f.template);
+        console.log("write file %s success!", filePath);
+    });
+}
+
+function assertNotExists(dirName) {
+    if (fs.existsSync(currentDir + dirName)) {
+        console.error("error: dir %s exists", dirName);
+        process.exit(1);
+    }
+}
+
+function assertNotEmptyArg(dirName) {
+    if (!dirName) {
+        console.error("dir is required! usage: demo.js <dir>");
+        process.exit(1);
+    }
+}
---
+++ step-by-step/task5-script/demo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo '<!DOCTYPE>
+ <title>Hello</title>
+ <h1>Hi</h1>' > index.html
+  echo 'h1{color: red;}' > css/style.css
+  echo 'var string = "Hello World"
+ alert(string)' > js/main.js
+  # touch index.html css/style.css js/main.js
+  echo 'success'
+  exit
+fi
